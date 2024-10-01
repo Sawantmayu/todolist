f6e479017fc54885cbc93cbe88b1bec5e7facb37
Sawantmayu <138888052+Sawantmayu@users.noreply.github.com>
2024-10-01 12:16:49 +0000

    c

--- app/add.php
+++ app/add.php
@@ -1,24 +1,25 @@
 <?php
 
-if(isset($_POST['title'])){
-    require '../db_conn.php';
+if (isset($_POST['title'])) {
+    require '../db_conn.php';  // Ensure the path is correct
 
-    $title = $_POST['title'];
+    $title = trim($_POST['title']);  // Trim any whitespace around the title
 
-    if(empty($title)){
-        header("Location: ../index.php?mess=error");
-    }else {
+    if (empty($title)) {
+        $message = "Error: Title cannot be empty!";
+    } else {
+        // Correct SQL syntax: use "VALUES" instead of "VALUE"
         $stmt = $conn->prepare("INSERT INTO todos(title) VALUES(?)");
         $res = $stmt->execute([$title]);
 
-        if($res){
-            header("Location: ../index.php?mess=success"); 
-        }else {
-            header("Location: ../index.php");
+        if ($res) {
+            $message = "Todo item added successfully!";
+        } else {
+            $message = "Error: Failed to add todo item.";
         }
-        $conn = null;
-        exit();
+
+        $conn = null;  // Close the database connection
     }
-}else {
-    header("Location: ../index.php?mess=error");
+} else {
+    $message = "Error: No data received!";
 }
